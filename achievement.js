7dfebf37c80fe7d053d23e8fcf8f61de625a4228
Kame-Bashuna <basshunakameroba@gmail.com>
2024-04-29 19:58:34 +0300

    add file

---
+++ achievement.js
@@ -0,0 +1,16 @@
+const achievementContainer = document.querySelector(".achievement-container");
+const addButton = document.querySelector(".btn");
+let achievement = document.querySelectorAll(".input-box");
+addButton.addEventListener("click", ()=>{
+let inputBox = document.createElement("p")
+let img = document.createElement("img")
+inputBox.className = "input-box"
+inputBox.setAttribute("contenteditable", "true");
+img.src = "images/buttons.png"
+achievementContainer.appendChild(inputBox).appendChild(img);
+})
+achievementContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+      e.target.parentElement.remove();
+    }
+})
